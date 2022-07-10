5ab12cbb3dff59db0b958d2ae30c1d9e3bfcb74b
nikitha mashetti <nikithamashetti434@gmail.com>
2022-07-10 13:04:01 +0530

    adding to file to git

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+let  b =document.getElementById('btn');
+
+b.addEventListener('click',() =>{
+    let n =document.getElementById("guess").value; //intput nuber
+   
+    let r =Math.random()*20;  //random nuber
+    let p =document.getElementById("respond");
+
+
+if(Math.abs(n,r)<= 5){ //check
+  
+p.innerText="Hot";
+}
+else{
+    p.innerText="Cold"
+}
+
+});
